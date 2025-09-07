e1c03ff2501dd971103b7edbbdcffbbb54d674f0
viakisun <kisun@viasofts.com>
2025-09-08 03:24:41 +0900

    fix: Resolve import path errors
    - Fix dashboard_page.dart to use correct package name 'dangam_app'
    - All URI target errors should now be resolved
    - MobileLayout and DashboardBody classes should now be found correctly

--- lib/pages/dashboard_page.dart
+++ lib/pages/dashboard_page.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:farm_work_app/widgets/mobile_layout.dart';
-import 'package:farm_work_app/pages/dashboard_body.dart';
+import 'package:dangam_app/widgets/mobile_layout.dart';
+import 'package:dangam_app/pages/dashboard_body.dart';
 
 class DashboardPage extends StatelessWidget {
   const DashboardPage({super.key});
